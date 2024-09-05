bd5f0979151a6be83f79aed04e2ffb81e2436fce
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-05 19:40:18 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,22 @@
+/* You are making an app that keeps track of things in the supermarket
+Declare a variable under each comment below stating:
+ 
+1) The most appropriate type 
+2) An appropriate name
+3) An initial value of your choosing 
+
+*/
+
+console.log("Test!");
+ 
+// The title of the app
+
+// The number of users who have downloaded the app
+
+// The number of apples in stock
+
+// The number of bananas in stock.
+
+// The number of checkouts in the supermarket. 
+
+// The name of the warehouse worker working today 
