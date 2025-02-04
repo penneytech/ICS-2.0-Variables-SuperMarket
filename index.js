0ed7be64c679be3235a7448e7a136342dde58597
penneytech <penneyp@hdsb.ca>
2025-02-04 09:42:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 /* You are making an app that keeps track of things in the supermarket
 Declare a variable under each comment below stating:
  
-1) The most appropriate type 
+1) The most appropriate type (let or const)
 2) An appropriate name
 3) An initial value of your choosing 
 
 */
-
-console.log("Test!");
  
 // The title of the app
+const appTitle = "My Supermarket App!";
 
 // The number of users who have downloaded the app
+let numberDownloads = 653;
 
 // The number of apples in stock
 
+
 // The number of bananas in stock.
+
 
 // The number of checkouts in the supermarket. 
 
+
 // The name of the warehouse worker working today 
+
+
+// The number of delivery trucks running today
+
+
+// The phone number of the supermarket
